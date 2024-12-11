2dc730e408a86f528efc435cac1e8048d5ac990e
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2024-12-11 20:20:44 +0000

    Configurar o Prettier scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>O amor só dá trabalho se você tentar escondê-lo. ♥️</h1>
+  return <h1>O amor só dá trabalho se você tentar escondê-lo. ♥️</h1>;
 }
 
 export default Home;
